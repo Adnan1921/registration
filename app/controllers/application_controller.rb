cbd35db49199ace021e53b26f9c53e6553ecc211
bruno <bruno.pawnda@gmail.com>
2020-08-19 20:34:30 +0200

    ide svega

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :fetch_menu
   def fetch_menu 
-    @menu = ["partners","cars","registrations", "users"]
+    @menu = ["partners","cars", "users"]
   end
  
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get'pages/home'
-  devise_for :users
-  get'home/index'
+  #get'pages/home'
+  
+  #devise_for :users
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+  
+  #get'home/index'
   root to: 'partners#index'
   resources :users
   resources :cars
-  resources :registrations
+  #resources :registrations
   resources :partners
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
