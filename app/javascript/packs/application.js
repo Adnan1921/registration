6881d9f0857f8fb7b6f44f0faa79d2ed0fb4c595
bruno <bruno.pawnda@gmail.com>
2020-08-25 11:35:21 +0200

    added jquery, bootstrap kako teba i logout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+window.jQuery = $;
+window.$ = $;
+
+
 import "bootstrap"
-require("./bootstrap")
+require("bootstrap")
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
