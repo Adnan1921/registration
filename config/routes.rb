e52d1630443d383e506a6566622c8dc92cdf4a0e
bruno <bruno.pawnda@gmail.com>
2020-08-31 16:13:23 +0200

    ed

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,14 @@ Rails.application.routes.draw do
   #devise_for :users
   devise_for :users
   #get'home/index'
-root :to => redirect("/users/sign_up")
+
 
   resources :users
   resources :cars
+  
+  root :to => redirect("/users/sign_up")
+  
+  
   #resources :registrations
   resources :partners
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
