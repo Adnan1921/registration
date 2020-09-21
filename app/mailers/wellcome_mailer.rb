c585f37ecaf43a7ffd44bc6818621dd9f7f02ab0
Adnan1921 <Adnan.biber2@gmail.com>
2020-09-21 22:36:41 +0200

    123

--- app/mailers/wellcome_mailer.rb
+++ app/mailers/wellcome_mailer.rb
@@ -2,6 +2,6 @@ class WellcomeMailer < ApplicationMailer
   def new_order_email
      @user = params[:user]
 
-     mail(to: railstest18@gmail.com, subject: "You got a new user!")
+     #mail(to: railstest18@gmail.com, subject: "You got a new user!")
    end
 end
